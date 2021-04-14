1adf2c3c41997a57f765c112fa86d910669e78f6
Erich Sauer <erich.sauer@gmail.com>
2021-04-14 16:32:43 -0700

    sets up initial file structure

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,10 @@
-const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
-const request = require('supertest');
-const app = require('../lib/app');
+// const pool = require('../lib/utils/pool');
+// const setup = require('../data/setup');
+// const request = require('supertest');
+// const app = require('../lib/app');
 
-describe('latergram routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
-});
+// describe('latergram routes', () => {
+//   beforeEach(() => {
+//     return setup(pool);
+//   });
+// });
